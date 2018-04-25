8cc1916ee2cacda880950400e35f54a197de01a8
jisajc9 <jisajc9>
2018-04-25 14:04:55 -0500

    Well, it works. Needs some refactoring.

--- Assignments/Course Exercise 1/app.js
+++ Assignments/Course Exercise 1/app.js
@@ -4,45 +4,76 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: [],
 
-        playerMaxDamage: 10,
-        playerMinDamage: 3,
+        playerDamage: {
+            maxDamage: 10,
+            minDamage: 3
+        },
 
-        monsterMaxDamage: 12,
-        monsterMinDamage: 5
+        playerSpecialDamage: {
+            maxDamage: 20,
+            minDamage: 10
+        },
+
+        playerHealAmt: 10,
+
+        monsterDamage: {
+            maxDamage: 12,
+            minDamage: 5
+        }
     },
     methods: {
         startGame: function() {
           this.gameIsRunning = true
           this.playerHealth = 100
           this.monsterHealth = 100
+            this.turns = []
         },
-        attack: function() {
-            this.monsterHealth -= this.calculateDamage(
-                this.playerMinDamage, this.playerMaxDamage
+        attack: function(doSpecial) {
+            var damage = this.calculateDamage(
+                doSpecial ? this.playerSpecialDamage : this.playerDamage
             )
-            if (this.monsterHealth < 0) his.monsterHealth = 0
+            this.monsterHealth -= damage
+            if (this.monsterHealth < 0) this.monsterHealth = 0
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player ${doSpecial ? "casts special attack on" : "hits"} Monster for ${damage} pts`
+            })
 
             if (this.checkWin()) return
 
-            this.playerHealth -= this.calculateDamage(
-                this.monsterMinDamage, this.monsterMaxDamage
-            )
-            if (this.playerHealth < 0) this.playerHealth = 0
+            this.monsterAttacks()
 
             this.checkWin()
         },
-        specialAttack: function() {
-
-        },
         heal: function() {
-
+            if (this.playerHealth <= 100 - this.playerHealAmt) {
+                this.playerHealth += this.playerHealAmt
+            } else {
+                this.playerHealth = 100
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player heals ${this.playerHealAmt} pts`
+            })
+            this.monsterAttacks()
+            this.checkWin()
         },
         giveUp: function() {
-
+            this.gameIsRunning = false
         },
-        calculateDamage: function(min, max) {
-            return Math.max(min, Math.floor(Math.random() * max) + 1)
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(this.monsterDamage)
+            this.playerHealth -= damage
+            if (this.playerHealth < 0) this.playerHealth = 0 // not ideal
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage} pts`
+            })
+        },
+        calculateDamage: function(damageObj) {
+            return Math.max(damageObj.minDamage, Math.floor(Math.random() * damageObj.maxDamage) + 1)
         },
         checkWin: function() {
             if (this.monsterHealth <= 0 || this.playerHealth <= 0) {
